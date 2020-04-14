0e99d933be043921addd517d067d4d9464a345ee
RicardoOno <ricardotadashiono@gmail.com>
2020-04-13 21:54:47 -0300

    Validando a data

--- nivel02/primeiro-projeto-node/src/routes/appointments.routes.ts
+++ nivel02/primeiro-projeto-node/src/routes/appointments.routes.ts
@@ -1,15 +1,36 @@
 import { Router } from 'express';
 import { uuid } from 'uuidv4';
+import { startOfHour, parseISO, isEqual } from 'date-fns';
 
 const appointmentsRouter = Router();
 
-const appointments = [];
+interface Appointment {
+  id: string;
+  provider: string;
+  date: Date;
+}
+
+const appointments: Appointment[] = [];
 
 // Como já foi definido a rota, não há necessidade de add toda a rota /appointments/, e sim só o /
 appointmentsRouter.post('/', (req, res) => {
   const { provider, date } = req.body;
 
-  const appointment = { id: uuid(), provider, date };
+  // zerando os minutos e segundos
+  const parseDate = startOfHour(parseISO(date));
+
+  // variicando se não há ja um agendamento na mesma hora
+  const findAppointmentInSameDate = appointments.find(a =>
+    isEqual(parseDate, a.date),
+  );
+
+  if (findAppointmentInSameDate) {
+    return res
+      .status(400)
+      .json({ error: 'This appointment is already booked' });
+  }
+
+  const appointment = { id: uuid(), provider, date: parseDate };
 
   appointments.push(appointment);
 
