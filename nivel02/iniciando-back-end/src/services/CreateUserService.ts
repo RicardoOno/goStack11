28f7df697a94c4f2b31591b8afd169011e72d86b
RicardoOno <ricardotadashiono@gmail.com>
2020-04-18 23:31:45 -0300

    Criptografia de senha

--- nivel02/iniciando-back-end/src/services/CreateUserService.ts
+++ nivel02/iniciando-back-end/src/services/CreateUserService.ts
@@ -1,4 +1,6 @@
 import { getRepository } from 'typeorm';
+import { hash } from 'bcryptjs';
+
 import User from '../models/Users';
 
 interface Request {
@@ -16,10 +18,12 @@ class CreateUserService {
       throw new Error('Email already used');
     }
 
+    const hashedPassword = await hash(password, 8);
+
     const user = userRepository.create({
       name,
       email,
-      password,
+      password: hashedPassword,
     });
 
     await userRepository.save(user);
