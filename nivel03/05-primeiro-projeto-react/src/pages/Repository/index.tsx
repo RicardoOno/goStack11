0136f34ef1379cd86944a69c66719055a63635ec
RicardoOno <ricardotadashiono@gmail.com>
2020-05-01 19:23:20 -0300

    Estilizando detalhe

--- nivel03/05-primeiro-projeto-react/src/pages/Repository/index.tsx
+++ nivel03/05-primeiro-projeto-react/src/pages/Repository/index.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
-import { useRouteMatch } from 'react-router-dom';
+import { useRouteMatch, Link } from 'react-router-dom';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+
+import { Header, RepositoryInfo, Issues } from './styles';
+import logoImg from '../../assets/github-logo.svg';
 
 interface RepositoryParams {
   repository: string;
@@ -8,7 +12,48 @@ interface RepositoryParams {
 const Repository: React.FC = () => {
   const { params } = useRouteMatch<RepositoryParams>();
 
-  return <h1>Repository:{params.repository} </h1>;
+  return (
+    <>
+      <Header>
+        <img src={logoImg} alt="Github Explore" />
+        <Link to="/">
+          <FiChevronLeft size={16} />
+          Voltar
+        </Link>
+      </Header>
+      <RepositoryInfo>
+        <header>
+          <img src="" alt="aaa" />
+          <div>
+            <strong>AAA</strong>
+            <p>bbbb</p>
+          </div>
+        </header>
+        <ul>
+          <li>
+            <strong>a</strong>
+            <span>b</span>
+          </li>
+          <li>
+            <strong>a</strong>
+            <span>b</span>
+          </li>
+          <li>
+            <strong>a</strong>
+            <span>b</span>
+          </li>
+        </ul>
+      </RepositoryInfo>
+      <Issues>
+        <Link to="weqwe">
+          <div>
+            <strong>asdasd</strong>
+            <p>asdadasdqwe</p>
+          </div>
+        </Link>
+      </Issues>
+    </>
+  );
 };
 
 export default Repository;
